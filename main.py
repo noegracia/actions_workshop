65477376773c444a9f46b7e6f6ec0f51ce696943
Noé Gracia <42318520+noegracia@users.noreply.github.com>
2024-08-01 10:17:43 +0200

    feat: new main python file

---
+++ main.py
@@ -0,0 +1,7 @@
+def increment(number):
+    return number + 1
+
+if __name__ == "__main__":
+    number = 0
+    incremented_number = increment(number)
+    print(f"The incremented number is: {incremented_number}")
