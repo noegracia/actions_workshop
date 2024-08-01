e44f1f8e430a9272a3c0f887aa5d2f10c28ccb39
Noé Gracia <42318520+noegracia@users.noreply.github.com>
2024-08-01 10:22:36 +0200

    feat: add docstring

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+main.py
+
+This module contains a simple function to increment a number and prints the result when run directly.
+"""
+
 def increment(num):
     """Increments the given number by 1."""
     return num + 1
