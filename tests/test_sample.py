f911a0adc5c00aac6a40839bf0dcc56c9b373887
noegracia <noe.gracia@aqsone.com>
2024-08-01 10:48:32 +0200

    fix: error import

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,6 @@
-"This modeule aim to run some unit test"
+"This module aim to run some unit test"
 
-from main import increment
+from ..main import increment
 
 def test_inc():
     """Run a unit test of the increment function"""
