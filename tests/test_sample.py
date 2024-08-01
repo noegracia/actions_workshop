f395fa80ae9199ffd2d2f50321d0f7628a03a247
noegracia <noe.gracia@aqsone.com>
2024-08-01 10:52:12 +0200

    fix: fixing error import, duplicating fun incr

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,8 @@
 "This module aim to run some unit test"
 
-from ..main import increment
+def increment(num):
+    """Increments the given number by 1."""
+    return num + 1
 
 def test_inc():
     """Run a unit test of the increment function"""
